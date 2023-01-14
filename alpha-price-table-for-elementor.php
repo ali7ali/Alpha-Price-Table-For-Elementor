9bd1be3a62c2fdee25416cf3f1ed8df1fed26b1b
ali7ali <ali7ali@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2023-01-14 20:30:21 +0000

    Update deprecated functions.
    git-svn-id: https://plugins.svn.wordpress.org/alpha-price-table-for-elementor/trunk@2848481 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- alpha-price-table-for-elementor.php
+++ alpha-price-table-for-elementor.php
@@ -1,19 +1,24 @@
 <?php
+
 /**
  * Plugin Name: Alpha Price Table For Elementor
  * Description: Premium Price Table for WordPress.
  * Author:      Ali Ali
  * Author URI:  https://github.com/Ali7Ali
- * Version:     1.0.1
+ * Version:     1.0.2
  * Text Domain: alpha-price-table-for-elementor
+ * Domain Path: /languages
  * License: GPLv3
  * 
- * WC tested up to: 6.4.1
- * Elementor tested up to: 3.6.4
- * Elementor Pro tested up to: 3.6.5
-*/
+ * WC tested up to: 7.3.0
+ * Elementor tested up to: 3.10.0
+ * Elementor Pro tested up to: 3.10.1
+ * 
+ * @package alpha-price-table-for-elementor
+ */
 
-/* Copyright 2021 Ali Ali (email : ali.abdalhadi.ali@gmail.com) 
+/*
+Copyright 2021 Ali Ali (email : ali.abdalhadi.ali@gmail.com) 
    
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -30,16 +35,17 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
 }
 
-define( 'ALPHAPRICETABLE_VERSION', '1.0.1' );
-define( 'ALPHAPRICETABLE_ADDONS_PL_ROOT', __FILE__ );
-define( 'ALPHAPRICETABLE_PL_URL', plugins_url( '/', ALPHAPRICETABLE_ADDONS_PL_ROOT ) );
-define( 'ALPHAPRICETABLE_PL_PATH', plugin_dir_path( ALPHAPRICETABLE_ADDONS_PL_ROOT ) );
-define( 'ALPHAPRICETABLE_PL_ASSETS', trailingslashit( ALPHAPRICETABLE_PL_URL.'assets' ) );
-define( 'ALPHAPRICETABLE_PL_INCLUDE', trailingslashit( ALPHAPRICETABLE_PL_PATH .'include' ));
-define( 'ALPHAPRICETABLE_PLUGIN_BASE', plugin_basename( ALPHAPRICETABLE_ADDONS_PL_ROOT ) );
+define('ALPHAPRICETABLE_VERSION', '1.0.2');
+define('ALPHAPRICETABLE_ADDONS_PL_ROOT', __FILE__);
+define('ALPHAPRICETABLE_PL_URL', plugins_url('/', ALPHAPRICETABLE_ADDONS_PL_ROOT));
+define('ALPHAPRICETABLE_PL_PATH', plugin_dir_path(ALPHAPRICETABLE_ADDONS_PL_ROOT));
+define('ALPHAPRICETABLE_PL_ASSETS', trailingslashit(ALPHAPRICETABLE_PL_URL . 'assets'));
+define('ALPHAPRICETABLE_PL_INCLUDE', trailingslashit(ALPHAPRICETABLE_PL_PATH . 'include'));
+define('ALPHAPRICETABLE_PLUGIN_BASE', plugin_basename(ALPHAPRICETABLE_ADDONS_PL_ROOT));
+
 // Required File
-include( ALPHAPRICETABLE_PL_INCLUDE.'/alpha-pricetable.php' );
+include(ALPHAPRICETABLE_PL_INCLUDE . '/class-alpha-price-table.php');
